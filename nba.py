b469c4d07b3c12b84ed83c30a3e0f4abe89e6ea9
VictorLMoura <victorlaperutademoura@hotmail.com>
2019-10-24 23:56:11 -0300

    de onde para onde done

--- nba.py
+++ nba.py
@@ -8,9 +8,12 @@ import json
 import os
 
 dict_signed = {}
+dict_waived = {}
 dict_re_signed = {}
 list_signed = []
 list_re_signed = []
+list_waived = []
+
 
 list_teams = ["Golden State Warriors", "LA Clippers","Boston Celtics", "Chicago Bulls", "Houston Rockets", "Cleveland Cavaliers", 
                 "Dallas Mavericks", "Los Angeles Lakers", "Brooklyn Nets", "Memphis Grizzlies", "Washington Wizards", "Oklahoma City Thunder", 
@@ -46,7 +49,7 @@ driver.get("https://stats.nba.com/transactions/")
 print(driver.current_url)
 assert "" in driver.current_url
 
-for i in range(40):
+for i in range(15):
     see_more = driver.find_element_by_xpath("//div[@class='button-container small-12 columns']/a[@class='button']")
 
     actions = ActionChains(driver)
@@ -58,6 +61,7 @@ for i in range(40):
 containers = driver.find_elements_by_xpath("//div[@class='transactions-list__date columns large-12']")
 transfers = driver.find_elements_by_xpath("//div[@class='transactions-list__description small-6 medium-5 large-6 columns']")
 years = []
+years_waived = []
 for i in range(len(containers)):
     print("Containeir: ",i+1)
     all_containers_transfers = []
@@ -69,16 +73,15 @@ for i in range(len(containers)):
     all_containers_transfers.append(last.text)
 
     j=1
-    print("             Coluna : ",j+1, "Ano:   ", year)
+    print("             Coluna : ",j+1, "Ano:   ", year.text)
     while(1):
         try:
-            print("             Coluna : ",j+1)
+            #print("             Coluna : ",j+1)
             arg1 ="//div[@class='transactions-list__date columns large-12']" + "[" + str(i+1) + "]"+ "/div[@class='small-12 large-10 columns']/div[@class='small-12 large-12 columns transactions-list__container']" + "[" + str(j) + "]"+ "/div[@class='transactions-list__description small-6 medium-5 large-6 columns']"
             container_transfer = driver.find_element_by_xpath(arg1)
             if(container_transfer):
                 all_containers_transfers.append(container_transfer.text)
                 j = j + 1
- 
             else:
                 break
         except Exception as e:
@@ -94,16 +97,21 @@ for i in range(len(containers)):
         elif("signed" in all_containers_transfers[i]):
             years.append(string) #Apenas os signed importam, então o 
                                 #tamanho dessas listas deve ser igual
-
+        elif("waived" in all_containers_transfers[i]):
+            print("waived")
+            years_waived.append(string) #Precisamos de uma lista sincronizada só para os waiveds
 for t in transfers:
     if "re-signed" in t.text:
         list_re_signed.append(t.text)
     elif "signed" in t.text:
         list_signed.append(t.text)
+    elif "waived" in t.text:
+        list_waived.append(t.text)
 
 try:
     os.remove("signed.json")
     os.remove("re_signed.json")
+    os.remove("waived.json")
 except:
     pass
 
@@ -123,9 +131,14 @@ with open("signed.json", 'w') as f:
         index = index + 1
     json.dump(dict_signed, f, indent=1)
 
-with open("re_signed.json", "w") as f:
-    for transfer in list_re_signed:
-        dict_json = parse_to_json(transfer, list_teams, all_players)
+with open("waived.json", "w") as f:
+    for transfer in list_waived:
+        dict_json = parse_to_json(transfer, list_teams, all_players) 
         if dict_json != -1:
-            dict_re_signed.update(dict_json)
-    json.dump(dict_re_signed, f, indent=1)
+            dict_waived.update(dict_json)
+
+    index = 0
+    for key in dict_waived:
+        dict_waived[key].append(years_waived[index])
+        index = index + 1
+    json.dump(dict_waived, f, indent=1)
